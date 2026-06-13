630f4ff97b26bf8fc275fc9289f01522fe060afb
Neelima <23BQ1A1262@vvit.net>
2026-06-13 14:18:47 +0530

    Time: 0 ms (100%), Space: 43.6 MB (41.71%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,7 +3,8 @@ class Solution {
         int k=0;
         for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]!=val){
+            if(nums[i]!=val)
+            {
                 nums[k]=nums[i];
                 k++;
             }
