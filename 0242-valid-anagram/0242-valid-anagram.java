8b23429fbc6600070cb1731b4c4fb14e41614935
Neelima <23BQ1A1262@vvit.net>
2026-06-15 13:15:59 +0530

    Time: 12 ms (29.33%), Space: 46.6 MB (15.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,10 +3,30 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        char[] arr1=s.toCharArray();
-        char[] arr2=t.toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        return Arrays.equals(arr1,arr2);  
+        HashMap<Character,Integer> h1 = new HashMap<>();
+        HashMap<Character,Integer> h2 = new HashMap<>();
+        
+        //populating the hashes
+        for(char c:s.toCharArray())
+        {
+            h1.put(c,h1.getOrDefault(c,0)+1);
+        }
+        for(char c:t.toCharArray())
+        {
+            h2.put(c,h2.getOrDefault(c,0)+1);
+        }
+        for(Map.Entry<Character,Integer> entry:h1.entrySet()){
+            char key = entry.getKey();
+            int val = entry.getValue();
+            if(!h2.containsKey(key))
+            {
+                return false;
+            }
+            if(val!=h2.get(key))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
