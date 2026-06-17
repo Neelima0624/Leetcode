a0e05ad19d568058277ef0afcf10ccbe8e0fa107
Neelima <23BQ1A1262@vvit.net>
2026-06-17 13:38:00 +0530

    Time: 2 ms (29.57%), Space: 45.5 MB (48.61%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int compress(char[] chars) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while(i<chars.length)
+        {
+            char ch = chars[i];
+            int count = 0;
+        while(i<chars.length && chars[i]==ch)
+        {
+            count++;
+            i++;
+        }
+        sb.append(ch);
+        if(count>1)
+        {
+             sb.append(count);
+        }
+        }
+        for(int j=0;j<sb.length();j++)
+        { 
+            chars[j] = sb.charAt(j);
+
+        }
+        return sb.length();
+
+        
+
+    }
+}
