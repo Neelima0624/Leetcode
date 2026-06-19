5e9bfe632d128435158e789490ba6a2fd45bbac3
Neelima <23BQ1A1262@vvit.net>
2026-06-19 10:01:30 +0530

    Time: 3 ms (60.63%), Space: 44.6 MB (49.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,23 +1,32 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase();
-        String c="";
-        for(int i=0;i<s.length();i++)
-        {
-            char ch = s.charAt(i);
-            if(Character.isLetterOrDigit(ch)){
-             c=c+ch;
+        int l = 0;
+        int r = s.length()-1;
+        while(l<=r){
+            while( l<r && !Character.isLetterOrDigit(s.charAt(l)))
+            {
+                l++;
             }
+            while(r>l && !Character.isLetterOrDigit(s.charAt(r)))
+            {
+                r--;
+            }
+            char l_c = s.charAt(l);
+            char r_c = s.charAt(r);
+        if(Character.isUpperCase(s.charAt(l)))
+        {
+             l_c = Character.toLowerCase(s.charAt(l));
         }
-        int left=0;
-        int right=c.length()-1;
-        while(left<right)
+        if(Character.isUpperCase(s.charAt(r)))
         {
-            if(c.charAt(left)!=c.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
+             r_c = Character.toLowerCase(s.charAt(r));
+        }
+        if(l_c != r_c)
+        {
+            return false;
+        }
+        l++;
+        r--;
         }
         return true;
     }
