15502995c3a802f513f6800fba6338314cea7dcf
Neelima <23BQ1A1262@vvit.net>
2026-06-23 09:29:24 +0530

    Time: 104 ms (5.14%), Space: 42.8 MB (8.36%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,13 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        for(int i=0;i<=x;i++)
+        {
+            if((long)i*i>x)
+            {
+                return i-1;
+            }
+        }
+        return x;
+
     }
 }
