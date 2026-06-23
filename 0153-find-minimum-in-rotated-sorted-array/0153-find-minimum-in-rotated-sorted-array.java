f5f0b7efcf73d951ccc1fab890dc91fca159ddf4
Neelima <23BQ1A1262@vvit.net>
2026-06-23 13:53:00 +0530

    Time: 0 ms (100%), Space: 43.8 MB (54.73%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,33 +1,19 @@
 class Solution {
     public int findMin(int[] arr) {
-
-    int pivot = findPivot(arr);
-
-    if(pivot == -1){
-        return arr[0];
-    }
-
-    return arr[pivot + 1];
-}
-       public int findPivot(int arr[]){
-        int s=0;
-        int e=arr.length-1;
-        while(s<=e){
-            int mid=(s+e)/2;
-            if(mid<e && arr[mid]>arr[mid+1]){
-                return mid;
-            }
-            if(mid>s && arr[mid-1]>arr[mid]){
-                return mid-1;
-            }
-            if(arr[s]<arr[mid]){
-                s=mid+1;
+        int low = 0;
+        int high = arr.length-1;
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(arr[mid]>=arr[high])
+            {
+                low = mid+1;
             }
             else{
-                e=mid-1;
+                high = mid;
             }
         }
-        return -1;
+        return arr[high];
     }
     
 }
