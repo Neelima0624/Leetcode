7db90d63976008478dc0488d661687882ee1f2ad
Neelima <23BQ1A1262@vvit.net>
2026-07-05 12:56:24 +0530

    Time: 2 ms (70.04%), Space: 55.9 MB (6.47%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int c = 0;
+        int can = 0;
+        for(int num:nums)
+        {
+            if(c==0){
+            can = num;
+            }
+            if(num==can)
+            {
+                c++;
+            }
+            else{
+                c--;
+            }
+        }
+        return can;
+        
+    }
+}
