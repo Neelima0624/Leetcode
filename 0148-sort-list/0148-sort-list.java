4870dee6ee06cd5e1c0495c464b5e9f72f335dff
Neelima <23BQ1A1262@vvit.net>
2026-07-25 13:32:47 +0530

    Time: 14 ms (23.72%), Space: 64.9 MB (6.29%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,22 +10,50 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-       ArrayList<Integer> arr = new ArrayList<>();
-       ListNode curr=head;
-       while(curr!=null)
-       {
-        arr.add(curr.val);
-        curr=curr.next;
-       }
-       Collections.sort(arr);
-        curr=head;
-        int i=0;
-        while(curr!=null)
-        {
-            curr.val=arr.get(i);
-            curr=curr.next;
-            i++;
-        }
+    if(head==null || head.next==null)
+    {
         return head;
+    }
+    ListNode left=null;
+    ListNode right=null;
+    if(head.next.next==null)
+    {
+        right=sortList(head.next);
+        head.next=null;
+        left=sortList(head);
+    }
+    else{
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&& fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode temp = slow.next;
+        slow.next=null;
+        left = sortList(head);
+        right=sortList(temp);
+    }
+    return mergeTwoLists(left,right);
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1==null)
+        {
+            return list2;
+        }
+        if(list2==null)
+        {
+            return list1;
+        }
+        if(list1.val<list2.val)
+        {
+           list1.next =  mergeTwoLists(list1.next,list2);
+            return list1;
+        }
+        else{
+            list2.next = mergeTwoLists(list1,list2.next);
+            return list2;
+        }
     }
 }
